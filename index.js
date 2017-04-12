436203d92a5821be9673f57a34285b51e321fa97
Gustavo Morales <gustavo.morales@morningstar.com>
2017-04-12 11:46:18 -0500

    Add error message 404 for no routes found

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const server = express();
 
 const config = require("./server/config");
 
-server.get('/', (req, res) => {
+server.get('/', (req, res, next) => {
   res.send('Hello World');
+});
+
+server.use( (req, res, next) => {
+  res.status(404);
+  res.json({
+    "message": "Error. Route not found"
+  });
 });
 
 server.listen(config.port, config.hostname, () => {
