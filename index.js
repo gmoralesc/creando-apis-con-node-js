349a9eebdceef59b3c1eaf214d740962367f8bb0
Gustavo Morales <gustavo.morales@morningstar.com>
2017-04-12 11:25:51 -0500

    Replace web server with express

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 "use strict";
 
-const http = require('http');
+const express = require('express');
+const server = express();
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+server.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
 server.listen(port, hostname, () => {
