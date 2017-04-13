758d233de0f8e36c9caa0ca05e19f27b32d547cf
Gustavo Morales <gustavo.morales@morningstar.com>
2017-04-12 22:47:53 -0500

    REST Layout and Boilerplate controller routes

---
+++ server/api/v1/posts/posts-controller.js
@@ -0,0 +1,21 @@
+"use strict";
+
+exports.all = (req, res, next) => {
+    res.json({});
+};
+
+exports.post = (req, res, next) => {
+    res.json({});
+};
+
+exports.get = (req, res, next) => {
+    res.json({});
+};
+
+exports.put = (req, res, next) => {
+    res.json({});
+};
+
+exports.delete = (req, res, next) => {
+    res.json({});
+};
--- server/api/v1/posts/posts-routes.js
+++ server/api/v1/posts/posts-routes.js
@@ -1,16 +1,23 @@
 "use strict";
 
-var router = require('express').Router();
+var router = require("express").Router();
+var controller = require("./posts-controller");
+
+/*
+ * /api/posts/     GET    - READ ALL
+ * /api/posts/     POST   - CREATE
+ * /api/posts/:id  GET    - READ ONE
+ * /api/posts/:id  PUT    - UPDATE
+ * /api/posts/:id  DELETE - DELETE
+ */
 
 router.route("/")
-    .get((req, res, next) => {
-        res.json([{
-            "id": 1,
-            "name": "Hello"
-        },{
-            "id": 2,
-            "name": "World"
-        }]);
-    });
+    .get(controller.all)
+    .post(controller.post);
+
+router.route("/:id")
+    .get(controller.get)
+    .put(controller.put)
+    .delete(controller.delete);
 
 module.exports = router;
