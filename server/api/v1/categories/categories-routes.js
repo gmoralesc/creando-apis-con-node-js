716621a349f9293f544dc78b9b2f333ccd1e78fd
Gustavo Morales <gustavo.morales@morningstar.com>
2017-04-13 21:25:51 -0500

    Create Bolierplate API for Users and Categories

---
+++ server/api/v1/categories/categories-routes.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var router = require("express").Router();
+var controller = require("./categories-controller");
+
+router.route("/")
+    .get(controller.all)
+    .post(controller.post);
+
+router.route("/:id")
+    .get(controller.get)
+    .put(controller.put)
+    .delete(controller.delete);
+
+module.exports = router;
--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,8 +1,12 @@
 "use strict";
 
 var router = require("express").Router();
+var userRoutes = require("./users/users-routes");
 var postRoutes = require("./posts/posts-routes");
+var categoryRoutes = require("./categories/categories-routes");
 
+router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
+router.use("/categories", categoryRoutes);
 
 module.exports = router;
