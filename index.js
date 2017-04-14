5f313bb35e277ad0a68043f583657f9e312ef59c
Gustavo Morales <gustavo.morales@morningstar.com>
2017-04-14 10:50:33 -0500

    Add express

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 "use strict";
 
-const http = require('http');
+const express = require('express');
+const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.get('/', (req, res) => {
+  res.send('Hello World\n');
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
