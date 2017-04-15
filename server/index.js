3e45c92972ccd34f0dd8b58d7764126ab8c61581
Gustavo Morales <gustavo.morales@morningstar.com>
2017-04-14 23:21:56 -0500

    Versioning API

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const morgan = require("morgan");
 const logger = require("winston");
 
-const api = require("./api");
+const api = require("./api/v1");
 
 const app = express();
 // Setup middleware
@@ -12,6 +12,7 @@ app.use(morgan("common"));
 
 // Setup router and routes
 app.use("/api", api);
+app.use("/api/v1", api);
 
 app.use( (req, res, next) => {
   logger.info("Route not found");
