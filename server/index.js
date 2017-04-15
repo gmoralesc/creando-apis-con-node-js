71a074afcc3e42870ac236989a96fe02bdd2f095
Gustavo Morales <gustavo.morales@morningstar.com>
2017-04-14 22:21:15 -0500

    Add route logger and cutsom log

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 "use strict";
 
 const express = require('express');
+const morgan = require('morgan');
+const logger = require('winston');
+
 const app = express();
+// Setup middleware
+app.use(morgan('common'));
 
 app.get('/', (req, res, next) => {
+  logger.info("Home page");
   res.json({
     "message": "Welcome to the API"
   });
 });
 
 app.use( (req, res, next) => {
+  logger.info("Route not found");
   res.status(404);
   res.json({
     "error": "Error. Route not found"
@@ -17,6 +24,7 @@ app.use( (req, res, next) => {
 });
 
 app.use( (err, req, res, next) => {
+  logger.error("Error");
   res.status(500);
   res.json({
     "error": `${err}`
