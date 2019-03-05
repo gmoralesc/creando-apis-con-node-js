7a4614196546a71530dbe85d0bd5ce37bdfbee97
Gustavo Morales <gustavo.morales@gmail.com>
2019-03-05 07:20:19 -0500

    Add Express JS and configuration file

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = require('./server');
+const config = require('./server/config');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+const { port } = config.server;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running at port: ${port}`);
 });
---
+++ server/config/index.js
@@ -0,0 +1,7 @@
+const config = {
+  server: {
+    port: 3000,
+  },
+};
+
+module.exports = config;
