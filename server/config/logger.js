fdead3ef4c7bd1588f7466460d49e1189578abb0
Gustavo Morales <gustavo.morales@gmail.com>
2019-03-05 11:14:08 -0500

    Add log header from requests

--- server/config/logger.js
+++ server/config/logger.js
@@ -1,9 +1,34 @@
 const { createLogger, format, transports } = require('winston');
+const morgan = require('morgan');
+const stripFinalNewline = require('strip-final-newline');
 
 // Setup logger
 const logger = createLogger({
   format: format.simple(),
   transports: [new transports.Console()],
 });
+
+// Setup requests logger
+morgan.token('id', req => req.id);
+
+const requestFormat = ':remote-addr [:date[iso]] :id ":method :url" :status';
+const requests = morgan(requestFormat, {
+  stream: {
+    write: (message) => {
+      // Remove all line breaks
+      const log = stripFinalNewline(message);
+      return logger.info(log);
+    },
+  },
+});
+
+// Attach to logger object
+logger.requests = requests;
+
+// Format as request logger and attach to logger object
+logger.header = (req) => {
+  const date = new Date().toISOString();
+  return `${req.ip} [${date}] ${req.id} "${req.method} ${req.originalUrl}"`;
+};
 
 module.exports = logger;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const morgan = require('morgan');
+const requestId = require('express-request-id')();
 
 const logger = require('./config/logger');
 
@@ -7,7 +7,8 @@ const logger = require('./config/logger');
 const app = express();
 
 // Setup middleware
-app.use(morgan('combined', { stream: { write: message => logger.info(message) } }));
+app.use(requestId);
+app.use(logger.requests);
 
 // Routes
 app.get('/', (req, res, next) => {
@@ -18,22 +19,19 @@ app.get('/', (req, res, next) => {
 
 // No route found handler
 app.use((req, res, next) => {
-  const message = 'Route not found';
-  const statusCode = 404;
-
-  logger.warn(message);
-
-  res.status(statusCode);
-  res.json({
-    message,
+  next({
+    message: 'Route not found',
+    statusCode: 404,
+    level: 'warn',
   });
 });
 
 // Error handler
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
+  const { message, statusCode = 500, level = 'error' } = err;
+  const log = `${logger.header(req)} ${statusCode} ${message}`;
 
-  logger.error(message);
+  logger[level](log);
 
   res.status(statusCode);
   res.json({
