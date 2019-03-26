51b31bd90c64b3d1c945d5af22f5519d6f90dcd9
Gustavo Morales <gustavo.morales@gmail.com>
2019-03-25 19:09:33 -0500

    Add Utils / sortCompactStr test

---
+++ server/utils/index.test.js
@@ -0,0 +1,18 @@
+const { sortCompactToStr } = require('./');
+
+describe('Utils', () => {
+  describe('sortCompactStr', () => {
+    test('Descendant', () => {
+      const result = sortCompactToStr('createdAt', 'desc');
+      const expected = '-createdAt';
+
+      expect(result).toEqual(expected);
+    });
+    test('Ascendant', () => {
+      const result = sortCompactToStr('createdAt', 'asc');
+      const expected = 'createdAt';
+
+      expect(result).toEqual(expected);
+    });
+  });
+});
