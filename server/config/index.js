2a876c7f454faebb065c38f52561abd048b0b2c0
Gustavo Morales <gustavo.morales@gmail.com>
2020-06-07 11:13:30 -0700

    Modularize Express and Add config file

---
+++ server/config/index.js
@@ -0,0 +1,9 @@
+// server/config/index.js
+
+const config = {
+  server: {
+    port: 3000,
+  },
+};
+
+module.exports = config;
--- server/index.js
+++ server/index.js
@@ -2,14 +2,10 @@
 
 const express = require('express');
 
-const port = 3000;
-
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-});
+module.exports = app;
