315afb1595365d7f51c1f31f8128fa96ca86d601
Gustavo Morales <gustavo.morales@gmail.com>
2020-08-22 14:33:06 -0700

    Add default CORS

--- server/index.js
+++ server/index.js
@@ -3,12 +3,22 @@
 const express = require('express');
 const requestId = require('express-request-id')();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const logger = require('./config/logger');
 const api = require('./api/v1');
 
 // Init app
 const app = express();
+
+// setup CORS
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
+  })
+);
 
 // Setup middleware
 app.use(requestId);
