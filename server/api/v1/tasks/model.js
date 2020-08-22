50719b48b94b0a736b6e70d71561427baf34abdf
Gustavo Morales <gustavo.morales@gmail.com>
2020-08-21 20:38:10 -0700

    Add Mongoose Schema and basic fields validation

--- server/api/v1/tasks/model.js
+++ server/api/v1/tasks/model.js
@@ -2,12 +2,38 @@
 
 const mongoose = require('mongoose');
 
-const task = {
-  title: String,
-  description: String,
-  completed: Boolean,
-  url: String,
-  dueDate: Date,
+const { Schema } = mongoose;
+
+const fields = {
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 128,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  description: {
+    type: String,
+    default: '',
+    trim: true,
+    maxlength: 255,
+  },
+  url: {
+    type: String,
+    default: '',
+    trim: true,
+  },
+  dueDate: {
+    type: Date,
+    default: null,
+  },
 };
+
+const task = new Schema(fields, {
+  timestamps: true,
+});
 
 module.exports = mongoose.model('task', task);
